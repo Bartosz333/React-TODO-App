03cbebbf17d516923939cdef3d737a7bafb5c7cc
Bartosz Kozaczkiewicz <bkozaczkiewicz@hotmail.com>
2023-05-28 21:06:37 +0200

    Add correct to strContains

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import Hero from './components/Hero/Hero.js';
 import SearchForm from './components/SearchForm/SearchForm.js';
 import List from './components/List/List.js';
 import Container from './components/Container/Container.js';
+import NavBar from './NavBar/NavBar.js';
 const App = () => {
   return (
-    <Container>
-      <Hero />
-      <SearchForm />
-      <List />
-    </Container>
+    <main>
+      <NavBar />
+      <Container>
+        <Hero />
+        <SearchForm />
+        <List />
+      </Container>
+    </main>
   );
 };
 export default App;

4d22b8db6f3ab8244e2f0653d62048d48945743b
Bartosz Kozaczkiewicz <bkozaczkiewicz@hotmail.com>
2023-06-07 07:57:29 +0200

    Add 404 Not Found page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import NavBar from './NavBar/NavBar.js';
 import { Home } from './Home/Home.js';
 import { About } from './components/About/About.js';
 import { Favorite } from './components/Favorite/Favorite.js';
+import { Routes, Route } from 'react-router-dom';
+import { NotFound } from './components/NotFound/NotFound.js';
 
 const App = () => {
   return (
     <main>
       <NavBar />
       <Container>
-        <Home />
-        <About />
-        <Favorite />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/favorite' element={<Favorite />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
       </Container>
     </main>
   );
